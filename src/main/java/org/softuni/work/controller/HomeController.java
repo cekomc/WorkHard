79f02cc653de5ff156f28d033f5ea49d379d98c0
unknown <cvetan.rosenov@abv.bg>
2018-04-09 22:19:00 +0300

    admin and project manager views

--- src/main/java/org/softuni/work/controller/HomeController.java
+++ src/main/java/org/softuni/work/controller/HomeController.java
@@ -63,4 +63,64 @@ public class HomeController {
     public ModelAndView businessCreteApp(){
         return new ModelAndView("business-create-app");
     }
+
+    @GetMapping("/career-home")
+    public ModelAndView careerHome(){
+        return new ModelAndView("career-home");
+    }
+
+    @GetMapping("/career-my-profile")
+    public ModelAndView careerMyProfile(){
+        return new ModelAndView("career-my-profile");
+    }
+
+    @GetMapping("/career-my-projects")
+    public ModelAndView carerMyProjects(){
+        return new ModelAndView("career-my-projects");
+    }
+
+    @GetMapping("/admin-home")
+    public ModelAndView adminHome(){
+        return new ModelAndView("admin-home");
+    }
+
+    @GetMapping("/admin-workers")
+    public ModelAndView adminWorkers(){
+        return new ModelAndView("admin-workers");
+    }
+
+    @GetMapping("/admin-all-projects")
+    public ModelAndView adminAllProjects(){
+        return new ModelAndView("admin-all-projects");
+    }
+
+    @GetMapping("/admin-worker-edit")
+    public ModelAndView adminWorkerEdit(){
+        return new ModelAndView("admin-worker-edit");
+    }
+
+    @GetMapping("/project-manager-home")
+    public ModelAndView projectManagerHome(){
+        return new ModelAndView("project-manager-home");
+    }
+
+    @GetMapping("/project-manager-list-projects")
+    public ModelAndView projectManagerListProjects(){
+        return new ModelAndView("project-manager-list-projects");
+    }
+
+    @GetMapping("/project-manager-workers")
+    public ModelAndView projectManagerWorkers(){
+        return new ModelAndView("project-manager-workers");
+    }
+
+    @GetMapping("/project-manager-edit-worker")
+    public ModelAndView projectManagerEditWorker(){
+        return new ModelAndView("project-manager-edit-worker");
+    }
+
+    @GetMapping("/project-manager-edit-project")
+    public ModelAndView projectManagerEditProject(){
+        return new ModelAndView("project-manager-edit-project");
+    }
 }
